6e6245c8999db9ef7fc8b05fdf4ef70c6d8052e7
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-06-24 16:48:15 -0500

    Added structure and styling

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,11 +1,22 @@
 import { Helmet } from 'react-helmet-async';
 
+import { CardsContainer } from './styles';
+import { useProjects } from '../../hooks/useProjects';
+import { ProjectCard } from '../../components/ProjectCard';
+
 export const Projects = () => {
+  const projects = useProjects();
+
   return (
     <>
       <Helmet>
         <title> {`Projects | Amaury Vasquez`} </title>
       </Helmet>
+      <CardsContainer>
+        {projects.map((item, i) => (
+          <ProjectCard data={item} key={item.title + i} />
+        ))}
+      </CardsContainer>
     </>
   );
 };
---
+++ src/pages/Projects/styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const CardsContainer = styled.div`
+  width: 100%;
+  height: auto;
+  margin-top: 20px;
+  border-radius: 3px;
+  padding: 10px;
+
+  & > * {
+    margin-bottom: 10px;
+  }
+`;
