2cc1f3581c0fbfa95d55e7cd5461080db4c89a04
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-06-24 16:47:53 -0500

    Created initial page

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,11 @@
+import { Helmet } from 'react-helmet-async';
+
+export const About = () => {
+  return (
+    <>
+      <Helmet>
+        <title> About | Amaury Vasquez </title>
+      </Helmet>
+    </>
+  );
+};
