50524897d98fb8ee8f873af4a7b7c39358a60132
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-06-24 16:46:41 -0500

    Added about route

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Layout } from './Layout';
 import { Home } from '../pages/Home';
+import { About } from '../pages/About';
 import { Contact } from '../pages/Contact';
 import { Projects } from '../pages/Projects';
 import { GlobalStyles } from '../styles/GlobalStyles';
@@ -15,6 +16,7 @@ const App = () => {
         <Layout>
           <Routes>
             <Route element={<Home />} path="/" />
+            <Route element={<About />} path="/about" />
             <Route element={<Contact />} path="/contact" />
             <Route element={<Projects />} path="/projects" />
           </Routes>
