91febc82d044467c32d0e22807bf6a9985769990
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-06-24 21:06:16 -0500

    Added items positioning and styling

--- src/pages/Projects/styles.tsx
+++ src/pages/Projects/styles.tsx
@@ -4,10 +4,11 @@ export const CardsContainer = styled.div`
   width: 100%;
   height: auto;
   margin-top: 20px;
-  border-radius: 3px;
+  border-radius: 5px;
   padding: 10px;
-
-  & > * {
-    margin-bottom: 10px;
+  display: grid;
+  grid-gap: 30px;
+  & > :last-child {
+    margin-bottom: 20px;
   }
 `;
