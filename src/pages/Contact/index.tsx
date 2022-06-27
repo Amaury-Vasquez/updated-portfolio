cd81411d4adf3362e023644fbfe2a9773c6c13fa
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-06-27 11:21:25 -0500

    Added contact hook

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,37 +1,10 @@
 import { Helmet } from 'react-helmet-async';
-import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
 
+import { useContact } from '../../hooks/useContact';
 import { ContactInfo, Email, Icons, Link, Message } from './styles';
 
-interface ContactIcon {
-  color: string;
-  Icon: React.FC;
-  link: string;
-}
-
 export const Contact = () => {
-  const contactData: ContactIcon[] = [
-    {
-      color: '#016de5',
-      Icon: FaFacebook,
-      link: 'https://www.facebook.com/amaury.vasquezenriquez/',
-    },
-    {
-      color: '#23292f',
-      Icon: FaGithub,
-      link: 'https://github.com/Amaury-Vasquez',
-    },
-    {
-      color: '#dd2a7b',
-      Icon: FaInstagram,
-      link: 'https://www.instagram.com/vasquezamaury/',
-    },
-    {
-      color: '#1d9bf1',
-      Icon: FaTwitter,
-      link: 'https://twitter.com/AmauryVasquez11',
-    },
-  ];
+  const contactData = useContact();
 
   return (
     <>
