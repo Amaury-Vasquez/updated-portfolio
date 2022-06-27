4492a3d30608d41874615bc7835930aac3b7f501
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-06-27 12:16:50 -0500

    Commented imgur route caching

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,24 +4,24 @@
 // import { CacheFirst } from "workbox-strategies/CacheFirst";
 // import { createHandlerForURL } from "workbox-precaching/createHandlerForURL";
 // import { ExpirationPlugin } from "workbox-expiration/ExpirationPlugin";
-import { ExpirationPlugin } from "workbox-expiration";
-import { precacheAndRoute } from "workbox-precaching";
-import { CacheFirst } from "workbox-strategies";
-import { registerRoute, NavigationRoute } from "workbox-routing";
+import { ExpirationPlugin } from 'workbox-expiration';
+import { precacheAndRoute } from 'workbox-precaching';
+import { CacheFirst } from 'workbox-strategies';
+import { registerRoute, NavigationRoute } from 'workbox-routing';
 
-NavigationRoute("/index.html");
+NavigationRoute('/index.html');
 precacheAndRoute(self.__WB_MANIFEST);
-registerRoute(
-  /^https?:\/\/www.imgur.com\/.*/,
-  new CacheFirst({
-    plugins: [
-      new ExpirationPlugin({
-        maxAgeSeconds: 24 * 60 * 60 * 30,
-      }),
-    ],
-  })
-);
+// registerRoute(
+//   /^https?:\/\/www.imgur.com\/.*/,
+//   new CacheFirst({
+//     plugins: [
+//       new ExpirationPlugin({
+//         maxAgeSeconds: 24 * 60 * 60 * 30,
+//       }),
+//     ],
+//   })
+// );
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
