07dda31524632753b9a29419635bdcc07a34cf94
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-06-28 16:12:13 -0500

    Added small tablet size

--- src/DeviceSize.ts
+++ src/DeviceSize.ts
@@ -1,8 +1,9 @@
 export const DeviceSize = {
-  smallMobile: 'max-width: 320px',
-  mobile: 'max-width: 500px',
-  tablet: 'max-width: 800px',
-  laptop: 'min-width: 1024px',
-  laptopL: 'min-width: 1440px',
-  desktop: 'min-width: 2560px',
+  smallMobile: '320px',
+  mobile: '500px',
+  smallTablet: '700px',
+  tablet: '1023px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px',
 };
