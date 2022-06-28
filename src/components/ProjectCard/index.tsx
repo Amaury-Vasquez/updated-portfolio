b48ef37c0d8b43830bc049d6202d0f7fc30f1b09
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-06-27 22:33:55 -0500

    Added loader for image

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,46 +1,66 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-import { Card, IconSpan, Image, LinkSpan, ProjectInfo } from './styles';
+import { Loader } from '../Loader';
 import { ProjectData } from '../../hooks/useProjects';
+import { useImageLoad } from '../../hooks/useImageLoad';
+import {
+  Card,
+  IconSpan,
+  Image,
+  LinkSpan,
+  Loading,
+  ProjectInfo,
+} from './styles';
+import { useLinkClick } from '../../hooks/useLinkClick';
 
 export const ProjectCard = (props: { data: ProjectData }) => {
-  const [loaded, setLoaded] = useState(false);
   const { data } = props;
   const { color, Icon, tool } = data.powered;
-
-  const onImageLoad = () => setLoaded(true);
+  const { img, loaded } = useImageLoad(data.imageLink);
+  const handleClick = useLinkClick();
 
   return (
-    <Card loaded={loaded}>
-      <Image
-        height={'200px'}
-        width="200px"
-        src={data.imageLink}
-        alt="game station"
-        onLoad={() => onImageLoad()}
-      />
-      <ProjectInfo>
-        <h3> {data.title} </h3>
-        <p> {data.description} </p>
-        <IconSpan color={color}>
-          {`Powered by: ${tool}`}
-          {<Icon />}
-        </IconSpan>
-        {data.link && (
+    <>
+      {!loaded && (
+        <Loading>
+          <Loader />
+        </Loading>
+      )}
+      <Card loaded={loaded}>
+        <Image height={'200px'} width="200px" src={img} alt="game station" />
+        <ProjectInfo>
+          <h3> {data.title} </h3>
+          <p> {data.description} </p>
+          <IconSpan color={color}>
+            {`Powered by: ${tool}`}
+            {<Icon />}
+          </IconSpan>
+          {data.link && (
+            <LinkSpan>
+              {'Link: '}
+              <a
+                onClick={(e) => handleClick(e)}
+                href={data.link}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {data.link}
+              </a>
+            </LinkSpan>
+          )}
           <LinkSpan>
-            {'Link: '}
-            <a href={data.link} target="_blank" rel="noreferrer">
-              {data.link}
+            {'Repositorie: '}
+            <a
+              onClick={(e) => handleClick(e)}
+              href={data.repositorie}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {data.repositorie}
             </a>
           </LinkSpan>
-        )}
-        <LinkSpan>
-          {'Repositorie: '}
-          <a href={data.repositorie} target="_blank" rel="noreferrer">
-            {data.repositorie}
-          </a>
-        </LinkSpan>
-      </ProjectInfo>
-    </Card>
+        </ProjectInfo>
+      </Card>
+    </>
   );
 };
