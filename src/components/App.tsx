d51d75844e299e5a67f5d08839aeac233a2be320
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-06-28 17:03:40 -0500

    Added lazy load

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,16 @@
 import { HelmetProvider } from 'react-helmet-async';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Layout } from './Layout';
-import { Home } from '../pages/Home';
-import { About } from '../pages/About';
-import { Contact } from '../pages/Contact';
-import { Projects } from '../pages/Projects';
+import { FallbackScreen } from './FallbackScreen';
 import { GlobalStyles } from '../styles/GlobalStyles';
+
+//Lazy imports
+const Home = lazy(() => import('../pages/Home'));
+const About = lazy(() => import('../pages/About'));
+const Contact = lazy(() => import('../pages/Contact'));
+const Projects = lazy(() => import('../pages/Projects'));
 
 const App = () => {
   return (
@@ -14,12 +18,14 @@ const App = () => {
       <GlobalStyles />
       <BrowserRouter>
         <Layout>
-          <Routes>
-            <Route element={<Home />} path="/" />
-            <Route element={<About />} path="/about" />
-            <Route element={<Contact />} path="/contact" />
-            <Route element={<Projects />} path="/projects" />
-          </Routes>
+          <Suspense fallback={<FallbackScreen />}>
+            <Routes>
+              <Route element={<Home />} path="/" />
+              <Route element={<About />} path="/about" />
+              <Route element={<Contact />} path="/contact" />
+              <Route element={<Projects />} path="/projects" />
+            </Routes>
+          </Suspense>
         </Layout>
       </BrowserRouter>
     </HelmetProvider>
