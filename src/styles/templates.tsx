fc907f3c18faa7e152f40cf0a130fef2cd8fc420
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-06-27 22:37:47 -0500

    Added article and text content template

--- src/styles/templates.tsx
+++ src/styles/templates.tsx
@@ -1,6 +1,30 @@
 import styled from 'styled-components';
 
+import { fadeIn } from './animation';
 import { DeviceSize } from '../DeviceSize';
+
+export const Article = styled.article`
+  width: 100%;
+  height: auto;
+  line-height: var(--line-height);
+  color: var(--gray-text);
+  text-align: center;
+
+  & > p {
+    font-size: 1.1rem;
+    font-weight: 300;
+  }
+
+  @media (${DeviceSize.mobile}) {
+    margin-top: 20px;
+  }
+`;
+
+export const BoldText = styled.h4`
+  text-align: center;
+  font-size: 1.1rem;
+  font-weight: 400;
+`;
 
 export const Page = styled.div`
   min-height: 100vh;
@@ -14,5 +38,29 @@ export const Page = styled.div`
 
   @media (${DeviceSize.mobile}) {
     width: 100vw;
+  }
+`;
+
+export const TextContent = styled.div`
+  ${fadeIn()};
+  margin: 0 auto;
+  margin-top: 30px;
+  height: auto;
+  background: rgba(200, 200, 200, 0.1);
+  /* border: 1px solid var(--gray); */
+  color: var(--gray-text);
+  border-radius: 5px;
+  text-align: center;
+  line-height: var(--line-height);
+
+  @media (${DeviceSize.laptop}) {
+    width: 98%;
+    padding: 30px 50px;
+  }
+
+  @media (${DeviceSize.mobile}) {
+    width: 100%;
+    padding: 20px 20px;
+    margin-top: 0;
   }
 `;
